50f56499565bbc9df62f48bb307a59be6a765cbc
ADemattia <127196391+ADemattia@users.noreply.github.com>
2024-12-16 12:07:54 +0100

    Update ExtendedWhitman.scala

--- lisa-sets/src/main/scala/lisa/automation/ExtendedWhitman.scala
+++ lisa-sets/src/main/scala/lisa/automation/ExtendedWhitman.scala
@@ -31,10 +31,32 @@ class ExtendedWhitman(axiomsSeq: Seq[Sequent]) {
     val success = (gamma, delta) match {
       case (phi, phi) => Left(SCProof(IndexedSeq(SC.Hypothesis(goal, phi)))) 
       case (_, _) if axioms contains goal => SCProof(IndexedSeq())
-      case (gamma, delta) => {
-        if isNegation(gamma) then
-          prove(Sequent())
-      }
+      case (gamma, delta) => 
+
+      case gamma != None, delta != None => (prove(gamma, none) || prove(none, delta)) 
+      
+      case (neg(psi)_left, delta) => prove(psi_right, delta) 
+      case (And(phi,psi)_left, delta) => (prove(phi_left, delta) || prove(psi_left, delta)) 
+      case (Or(phi,psi)_left, delta) => (prove(phi_left, delta) && prove(psi_left, delta)) 
+      
+      case (neg(psi)_right, delta) => (prove(psi_left, delta))
+      case (And(phi,psi)_right, delta) => (prove(phi_right, delta) && prove(psi_right, delta)) 
+      case (Or(phi,psi)_right, delta) => (prove(phi_right, delta) || prove(psi_right,delta)) 
+
+      case (gamma, neg(psi)_left) => prove(gamma, psi_left) 
+      case (gamma, And(phi,psi)_left) => (prove(gamma, phi_left) || prove(gamma, psi_left)) 
+      case (gamma, Or(phi,psi)_left) => (prove(gamma, phi_left) && prove(gamma,psi_left)) 
+      
+      case (gamma, neg(psi)_right) => (prove(gamma,psi_left))
+      case (gamma, And(phi,psi)_right) => (prove(gamma, phi_right) && prove(gamma, psi_right)) 
+      case (gamma, Or(phi,psi)_right) => (prove(gamma, phi_right) || prove(gamma, psi_right)) 
+
+      case AxFormula(x in AxFormula => (prove(gamma, x_right) && prove(x_left, delta)) || (prove(delta, x_right) && prove(x_left, gamma)) 
+
+      // my idea for handle left and right gives to prove a array (Formula: F, side: 0 - left, 1 - right) 
+      // when you analyze the formula you analyze side and F and the rebuild a new array like
+      // ((And(psi, phi), 0), delta) => (prove((psi, 0), delta)) || prove((phi,0), delta)) 
+      // I think you don't have to build new strange property for Formula and subformula because the side is at  total formula level 
     }
   }
 }
