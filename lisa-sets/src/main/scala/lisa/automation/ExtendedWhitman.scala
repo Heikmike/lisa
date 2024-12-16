0c189787388af03281302a72c684cf6d3b29f4e1
Heikel Jebali <heikel.jebali@epfl.ch>
2024-12-16 13:10:39 +0100

    Temp

--- lisa-sets/src/main/scala/lisa/automation/ExtendedWhitman.scala
+++ lisa-sets/src/main/scala/lisa/automation/ExtendedWhitman.scala
@@ -1,40 +1,57 @@
 import lisa.kernel.proof.SequentCalculus.Sequent
 import lisa.utils.K.{_, given}
+import lisa.kernel.fol.FOL.*
 import lisa.kernel.proof.SCProof
 
 class ExtendedWhitman(axiomsSeq: Seq[Sequent]) {
-  private def isNegation(formula: Formula): Boolean = {
-    return true
-  }
-
-  private def isDisjunction(formula: Formula): Boolean = {
-    return true
-  }
-
-  private def isConjunction(formula: Formula): Boolean = {
-    return true
-  }
-
   def prove(goal: Sequent): Either[SCProof, String] = {
     val axioms = axiomsSeq.toSet
     val axiomsFormulas = axioms map (_.left.head) union (axioms map (_.right.head))
-    val proven: Set[Sequent] = Set()
-    val visited = Set.empty[Sequent]
+    var proven: Set[Sequent] = Set()
+    var visited: Set[Sequent] = Set()
+    var steps: IndexedSeq[SCProofStep] = IndexedSeq()
 
-    if proven contains goal then return Left(SCProof(proven toIndexedSeq))
+    if proven contains goal then return Left(SCProof(steps))
     if visited contains goal then return Right("Cyclic proof")
 
-    visited += goal
-    val gamma = goal.left.head
-    val delta = goal.right.head
+    visited = visited union Set(goal)
+    val left = goal.left
+    val right = goal.right
+    val gamma = left.head
+    val delta = right.head
 
-    val success = (gamma, delta) match {
-      case (phi, phi) => Left(SCProof(IndexedSeq(SC.Hypothesis(goal, phi)))) 
-      case (_, _) if axioms contains goal => SCProof(IndexedSeq())
-      case (gamma, delta) => {
-        if isNegation(gamma) then
-          prove(Sequent())
-      }
-    }
+    if axioms contains goal then return Left(SCProof(IndexedSeq())) // Ax
+    if gamma == delta then // Hyp
+      val hyp = Hypothesis(goal, gamma)
+      steps = steps :+ hyp
+      return Left(SCProof(steps))
+
+    val success = (gamma, delta) match // success means that success has type Left(proof)
+      case(ConnectorFormula(Neg, phi), delta) => // LeftNot
+        val seq = Sequent(phi.toSet, Set(delta))
+        prove(seq)
+      case(ConnectorFormula(And, Seq(phi, psi)), delta) => // LeftAnd
+        val seq1 = Sequent(Set(phi), right)
+        val seq2 = Sequent(Set(psi), right)
+        val p1 = prove(seq1)
+        val p2 = prove(seq2)
+
+        // TODO: Add the proof steps to the steps list
+        if p1.isLeft then p1 else p2
+      case(ConnectorFormula(Or, Seq(phi, psi)), delta) => // LeftOr
+        val seq1 = Sequent(Set(phi), right)
+        val seq2 = Sequent(Set(psi), right)
+        val p1 = prove(seq1)
+        val p2 = prove(seq2)
+
+        if p1.isLeft && p2.isLeft then p1 else Right("Error")
+      case(gamma, ConnectorFormula(Neg, phi)) => // RightNot
+        val seq = Sequent(left, phi.toSet)
+
+    success match
+      case Left(proof) =>
+        proven = proven union Set(goal)
+        Left(proof)
+      case Right(error) => Right(error)
   }
 }
--- lisa-sets/src/main/scala/lisa/automation/WhitmanVariant.scala
+++ lisa-sets/src/main/scala/lisa/automation/WhitmanVariant.scala
@@ -3,7 +3,7 @@ import lisa.prooflib.Library
 import lisa.fol.FOL as F
 
 object WhitmanVariant extends ProofTatic {
-  def apply(using lib: Library, proof: lib.Proof)(bot: F.Sequent): proof.ProofTaticJudgement = {
+  def apply(using lib: Library, proof: lib.Proof)(premises)(bot: F.Sequent): proof.ProofTaticJudgement = {
     if (bot.right.size != 1)
       return proof.InvalidProofTactic("The goal must be of the form Γ |- s <= t.")
   }
