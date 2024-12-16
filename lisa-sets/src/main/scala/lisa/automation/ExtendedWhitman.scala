cadb546a500ba70c23487d0a8097d6d264020ba9
Heikel Jebali <heikel.jebali@epfl.ch>
2024-12-16 15:32:10 +0100

    A few cases missing

--- lisa-sets/src/main/scala/lisa/automation/ExtendedWhitman.scala
+++ lisa-sets/src/main/scala/lisa/automation/ExtendedWhitman.scala
@@ -3,60 +3,81 @@ import lisa.utils.K.{_, given}
 import lisa.kernel.fol.FOL.*
 import lisa.kernel.proof.SCProof
 
+// INFO: Consider moving those objects and classes lisa.kernel.fol.FormulaDefinitions
 sealed trait Annotation
 case object LeftAnnotation extends Annotation
 case object RightAnnotation extends Annotation
+case object NoneAnnotation extends Annotation
 
 case class AnnotatedFormula(formula: Formula, annotation: Annotation)
-
 
 class ExtendedWhitman(axioms: Set[(AnnotatedFormula, AnnotatedFormula)]) {
   def prove(gamma: AnnotatedFormula, delta: AnnotatedFormula): Either[SCProof, String] = {
     val axiomsFormulas = axioms flatMap { case (a, b) => Set(a.formula, b.formula) }
+    val goal = Sequent(Set(gamma.formula), Set(delta.formula))
     var proven: Set[(AnnotatedFormula, AnnotatedFormula)] = Set()
     var visited: Set[(AnnotatedFormula, AnnotatedFormula)] = Set()
     var steps: IndexedSeq[SCProofStep] = IndexedSeq()
 
-    if proven contains (gamma, delta) then return Left(SCProof(steps))
-    if visited contains (gamma, delta) then return Right("Cyclic proof")
+    if proven.contains((gamma, delta)) then return Left(SCProof(steps))
+    if visited.contains((gamma, delta)) then return Right("Cyclic proof")
 
-    visited = visited union Set(goal)
-    val left = goal.left
-    val right = goal.right
-    val gamma = left.head
-    val delta = right.head
-
-    if axioms contains goal then return Left(SCProof(IndexedSeq())) // Ax
-    if gamma == delta then // Hyp
-      val hyp = Hypothesis(goal, gamma)
-      steps = steps :+ hyp
-      return Left(SCProof(steps))
+    visited = visited ++ Set((gamma, delta))
 
     val success = (gamma, delta) match // success means that success has type Left(proof)
-      case(ConnectorFormula(Neg, phi), delta) => // LeftNot
-        val seq = Sequent(phi.toSet, Set(delta))
-        prove(seq)
-      case(ConnectorFormula(And, Seq(phi, psi)), delta) => // LeftAnd
-        val seq1 = Sequent(Set(phi), right)
-        val seq2 = Sequent(Set(psi), right)
-        val p1 = prove(seq1)
-        val p2 = prove(seq2)
+      case (AnnotatedFormula(phi, phiAnnot), AnnotatedFormula(psi, psiAnnot))
+      if (phi == psi && phiAnnot != psiAnnot && phiAnnot != NoneAnnotation && psiAnnot != NoneAnnotation) => // Hyp
+          val hyp = Hypothesis(goal, phi)
+          steps = steps :+ hyp
+          Left(SCProof(steps))
+      case _ if axioms.contains((gamma, delta)) => Left(SCProof(steps)) // Ax
 
-        // TODO: Add the proof steps to the steps list
+      // ==== Gamma cases ====
+      case (AnnotatedFormula(ConnectorFormula(Neg, phi), annot), delta) => // LeftNot
+        val reversedAnnot = annot match
+          case LeftAnnotation => RightAnnotation
+          case RightAnnotation => LeftAnnotation
+          case NoneAnnotation => NoneAnnotation
+
+        prove(AnnotatedFormula(phi.head, reversedAnnot), delta)
+
+      case (AnnotatedFormula(ConnectorFormula(And, Seq(phi, psi)), annot), delta) => // LeftAnd
+        val p1 = prove(AnnotatedFormula(phi, annot), delta)
+        val p2 = prove(AnnotatedFormula(psi, annot), delta)
+        // TODO: Add the proof steps
         if p1.isLeft then p1 else p2
-      case(ConnectorFormula(Or, Seq(phi, psi)), delta) => // LeftOr
-        val seq1 = Sequent(Set(phi), right)
-        val seq2 = Sequent(Set(psi), right)
-        val p1 = prove(seq1)
-        val p2 = prove(seq2)
 
-        if p1.isLeft && p2.isLeft then p1 else Right("Error")
-      case(gamma, ConnectorFormula(Neg, phi)) => // RightNot
-        val seq = Sequent(left, phi.toSet)
+      case (AnnotatedFormula(ConnectorFormula(Or, Seq(phi, psi)), annot), delta) => // LeftOr
+        val p1 = prove(AnnotatedFormula(phi, annot), delta)
+        val p2 = prove(AnnotatedFormula(psi, annot), delta)
+        // TODO: Add the proof steps
+        if p1.isLeft && p2.isLeft then Left(SCProof(steps)) else Right("Error")
+
+
+      // ==== Delta cases ====
+      case (delta, AnnotatedFormula(ConnectorFormula(Neg, phi), annot)) => // RightNot
+        val reversedAnnot = annot match
+          case LeftAnnotation => RightAnnotation
+          case RightAnnotation => LeftAnnotation
+          case NoneAnnotation => NoneAnnotation
+
+        prove(delta, AnnotatedFormula(phi.head, reversedAnnot))
+
+      case (gamma, AnnotatedFormula(ConnectorFormula(And, Seq(phi, psi)), annot)) => // RightAnd
+        val p1 = prove(gamma, AnnotatedFormula(phi, annot))
+        val p2 = prove(gamma, AnnotatedFormula(psi, annot))
+        // TODO: Add the proof steps
+        if p1.isLeft then p1 else p2
+
+      case (gamma, AnnotatedFormula(ConnectorFormula(Or, Seq(phi, psi)), annot)) => // RightOr
+        val p1 = prove(gamma, AnnotatedFormula(phi, annot))
+        val p2 = prove(gamma, AnnotatedFormula(psi, annot))
+        // TODO: Add the proof steps
+        if p1.isLeft && p2.isLeft then Left(SCProof(steps)) else Right("Error")
 
     success match
       case Left(proof) =>
-        proven = proven union Set(goal)
+        proven = proven ++ Set((gamma, delta))
         Left(proof)
       case Right(error) => Right(error)
 // =======
