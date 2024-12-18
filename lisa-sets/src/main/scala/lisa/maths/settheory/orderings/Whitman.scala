86fa724897ca139280bd60ac6a5c20a2fbd36cc4
Heikel Jebali <heikel.jebali@epfl.ch>
2024-12-18 20:13:59 +0100

    Proofs

--- lisa-sets/src/main/scala/lisa/maths/settheory/orderings/Whitman.scala
+++ lisa-sets/src/main/scala/lisa/maths/settheory/orderings/Whitman.scala
@@ -4,17 +4,26 @@ import lisa.automation.ExtendedWhitman
 import lisa.fol.FOL.*
 
 object Whitman extends lisa.Main {
-  val x = VariableFormula("x")
-  val u = VariableFormula("u")
-  val left = x ∧ (¬(x) ∨ u)
-  val right = ¬(x) ∨ u
-  val bottom = AnnotatedFormula(⊥, NoneAnnotation)
+  // ==== Example 3.1 from paper ====
+  // val x = VariableFormula("x")
+  // val u = VariableFormula("u")
+  // val left = x ∧ (¬(x) ∨ u)
+  // val right = ¬(x) ∨ u
+  // val bottom = AnnotatedFormula(⊥, NoneAnnotation)
 
-  val axiom = (bottom, AnnotatedFormula(left, RightAnnotation))
-  val axiomSet: Set[(AnnotatedFormula, AnnotatedFormula)] = Set(axiom)
-  val gamma = bottom
-  val delta = AnnotatedFormula(u, RightAnnotation)
+  // val axiom = (bottom, AnnotatedFormula(left, RightAnnotation))
+  // val axiomSet: Set[(AnnotatedFormula, AnnotatedFormula)] = Set(axiom)
+  // val gamma = bottom
+  // val delta = AnnotatedFormula(right, RightAnnotation)
 
+  // ==== De Morgan's Law ====
+  // val phi = VariableFormula("phi")
+  // val psi = VariableFormula("psi")
+  // val axiomSet: Set[(AnnotatedFormula, AnnotatedFormula)] = Set()
+  // val gamma = AnnotatedFormula(¬(phi ∧ psi), LeftAnnotation)
+  // val delta = AnnotatedFormula(¬(phi) ∨ ¬(psi), RightAnnotation)
+
+  // ==== Transitivity ====
   // val x = VariableFormula("x")
   // val y = VariableFormula("y")
   // val z = VariableFormula("z")
@@ -25,6 +34,21 @@ object Whitman extends lisa.Main {
 
   // val gamma = AnnotatedFormula(x, LeftAnnotation)
   // val delta = AnnotatedFormula(z, RightAnnotation)
+  
+
+  // ==== Example 3.2 ====
+  // val phi = VariableFormula("phi")
+  // val psi = VariableFormula("psi")
+  // val theta = VariableFormula("theta")
+  // val etha = VariableFormula("etha")
+
+  // val f1 = psi ∧ theta
+  // val axiom1 = (AnnotatedFormula(phi, LeftAnnotation), AnnotatedFormula(f1, RightAnnotation))
+  // val axiom2 = (AnnotatedFormula(theta, LeftAnnotation), AnnotatedFormula(etha, RightAnnotation))
+  // val axiom3 = (AnnotatedFormula(psi, LeftAnnotation), AnnotatedFormula(¬(etha), RightAnnotation))
+  // val axiomSet = Set(axiom1, axiom2, axiom3)
+  // val gamma = AnnotatedFormula(phi, LeftAnnotation)
+  // val delta = AnnotatedFormula(¬(phi), RightAnnotation)
 
   val extendedWhitman = ExtendedWhitman(axiomSet)
   if extendedWhitman.prove(gamma, delta) then
